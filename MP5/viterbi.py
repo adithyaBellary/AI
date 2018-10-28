dc242d09acc24a70f5f9844e2305fffe76904427
hrege <davereg2@illinois.edu>
2018-10-27 20:12:43 -0500

    Finished baseline algorithm

--- MP5/viterbi.py
+++ MP5/viterbi.py
@@ -22,6 +22,44 @@ output: list of sentences, each sentence is a list of (word,tag) pairs.
 '''
 def baseline(train, test):
     predicts = []
+    train_dict = dict([('', dict([('', 0)]))])
+
+    # train_dict['the'] = {}
+    # train_dict['the']['NOUN'] = 1
+    # print(train_dict)
+
+    # train_dict['the']['NOUN'] += 1
+    # print(train_dict)
+
+    # # train_dict['the']['VERB'] = {}
+    # train_dict['the']['VERB'] = 4
+    # print(train_dict)
+
+    # tag = max(train_dict['the'], key = train_dict['the'].get)
+    # print(tag)
+
+    for sentence in train:
+    	for tag in sentence:
+    		# print(tag)
+    		if tag[0] not in train_dict:		#if we haven't seen word yet, create new dictionary for POS and count
+    			train_dict[tag[0]] = {}
+    			train_dict[tag[0]][tag[1]] = 1
+    		else:
+    			if(tag[1] not in train_dict[tag[0]]):	#if we haven't seen tag for that word yet, add that POS into dictionary
+    				train_dict[tag[0]][tag[1]] = 1
+    			else:
+    				train_dict[tag[0]][tag[1]] += 1
+
+    for sentence in test:
+    	temp_list = []
+    	for word in sentence:
+    		if word in train_dict:
+    			tag = max(train_dict[word], key = train_dict[word].get)		#Find key of most frequently occurring POS
+    		else:															#assign tag to unknown word
+    			tag = 'NOUN'
+    		temp_list.append((word, tag))
+    	predicts.append(temp_list)
+    
     return predicts
 
 '''
